5756e9725c93c85f201c098156213adb20b35f18
Épica Digital UY <epicadigitaluy@gmail.com>
2025-10-14 20:53:21 -0300

    Update index.js
    Yo que se, falta lo que te guste

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ dotenv.config({ path: "./src/.env" });
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
